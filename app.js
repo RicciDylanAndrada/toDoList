065300065918ad7e6113f74f228a214323c942e0
Dylan Andrada <dylanandrada@Dylans-MacBook-Pro.local>
2021-07-21 20:30:40 -0400

    added database feature and removal feature

--- app.js
+++ app.js
@@ -3,60 +3,99 @@
 const ejs = require("ejs");
 const express = require("express");
 const bodyParser = require("body-parser");
-const app =express();
+const mongoose = require("mongoose");
 
-const date = require(__dirname+"/date.js");
+const app = express();
 
-var items=[]
-let workItems=[]
-app.set("view engine","ejs");
 
-app.use(bodyParser.urlencoded({extended:true}))
+const date = require(__dirname + "/date.js");
+
+//var items=[]
+let workItems = []
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(express.static("public"))
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+}, {
+  useUnifiedTopology: true
+});
+const itemsSchema = {
+  name: String
+}
+const Item = mongoose.model("item", itemsSchema);
+
+const item1 = new Item({
+  name: "Book1"
+});
+const item2 = new Item({
+  name: "Book2"
+});
+const item3 = new Item({
+  name: "Book3"
+});
+const defaultItems = [item1, item2, item3]
 
 
-app.get("/",function(req,res){
 
-  let day  = date.getDate();
-
-
-
-
-
-  res.render("list",{listTitle:day,newListItem:items});
-
-})
-app.post("/",function(req,res){
-  var item = req.body.newItem;
-
-
-
-  if(req.body.list =="Work"){
-    workItems.push(item)
-    res.redirect("/work")
-  }else{
-    items.push(item);
-    res.redirect("/")
+app.get("/", function(req, res) {
+  Item.find({}, function(err, foundItems) {
+    if (foundItems.length == 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Succesfull");
+        }
+      });
+      res.redirect("/")
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItem: foundItems
+      });
+    };
+  });
+});
 
 
-  }
+
+
+
+
+
+app.post("/", function(req, res) {
+
+
+  var itemName = req.body.newItem;
+  const newItem = new Item({
+    name:itemName
+  });
+  newItem.save()
+  res.redirect("/");
+
 
 });
-app.post("/removeTask",function(req,res){
-  if (req.body.list ==="Work"){
+app.post("/removeTask", function(req, res) {
+  if (req.body.list === "Work") {
     workItems = [];
     res.redirect("/work");
 
+  } else {
+    items = [];
+    res.redirect("/");
   }
-  else {
-  items = [];
-  res.redirect("/");
-}
 })
 
-app.get("/work",function(req,res){
-  res.render("list",{listTitle:"Work List", newListItem:workItems});
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItem: workItems
+  });
 
 })
 // app.post("/work",function(req,res){
@@ -65,9 +104,17 @@ app.get("/work",function(req,res){
 //   res.redirect("/")
 // })
 
+app.post("/delete",function(req,res){
+  const checkedItem = (req.body.checkBox);
+  Item.findByIdAndRemove(checkedItem,function(err){
+    if(!err){
+      console.log("Succesuffl Delete");
+      res.redirect("/")
+    }
+  })
+});
 
 
-
-app.listen(3000,function(req,res){
+app.listen(3000, function(req, res) {
   console.log("Listening");
 })
